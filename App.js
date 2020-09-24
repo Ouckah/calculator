0a5514425d3d6e39179aea8683edbad2deb20d80
MushroomPlays <43829909+MushroomPlays@users.noreply.github.com>
2020-09-23 20:11:01 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CalculatorScreen from './screens/CalculatorScreen';
+
+export default class App extends React.Component {
+  render() {
+      return <CalculatorScreen />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
